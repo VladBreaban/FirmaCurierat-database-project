138c887c4782bdcaf78a558ad3312e536649b446
VladBreaban <vlad.breaban@relevance.ro>
2020-12-09 19:42:00 +0000

    added drivers page

---
+++ FirmaCurierat/Pages/Counter.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaCurierat.Pages
+{
+    public class CounterComponent:ComponentBase
+    {
+        public List<Models.FirmaCurierat.Soferi> drivers;
+
+        protected DataBaseManagement.DataManagement dataHelper
+        {
+            get;
+            set;
+        }
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            dataHelper = new DataBaseManagement.DataManagement();
+            // SqlConnection scn = new SqlConnection();
+            string ConnectionString = @"Data Source=DESKTOP-I3NIEPL\SQLEXPRESS;Initial Catalog=login_database;database=CurieratVladProiect;integrated security=SSPI";
+            drivers = new List<FirmaCurierat.Models.FirmaCurierat.Soferi>();
+            string sqlCommand = "select * from  soferi";
+            drivers = await dataHelper.LoadData<FirmaCurierat.Models.FirmaCurierat.Soferi, dynamic>(sqlCommand, new { }, ConnectionString);
+        }
+
+        protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, dynamic data)
+        {
+
+            try
+            {
+                if (await DialogService.Confirm("Are you sure you want to delete this record?") == true)
+                {
+                    
+                }
+            }
+            catch (System.Exception rlvMailerDeleteMailException)
+            {
+                NotificationService.Notify(NotificationSeverity.Error, $"Error", $"Unable to delete Mail");
+            }
+        }
+    }
+}
