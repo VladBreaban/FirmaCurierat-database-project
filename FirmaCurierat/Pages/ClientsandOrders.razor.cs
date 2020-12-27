8b204f183bbef6a2ee932ce747222cd5b1383721
VladBreaban <vlad.breaban@relevance.ro>
2020-12-27 22:00:01 +0000

    delete from db stuff

--- FirmaCurierat/Pages/ClientsandOrders.razor.cs
+++ FirmaCurierat/Pages/ClientsandOrders.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Radzen;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,6 +40,44 @@ namespace FirmaCurierat.Pages
             
             NavigationManager.NavigateTo("/addOrdersAndClients");
             await InvokeAsync(() => { StateHasChanged(); });
+        }
+        protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, object data)
+        {
+
+            try
+            {
+                if (await DialogService.Confirm("Do you want to delete this record?") == true)
+                {
+                  //  List<Models.FirmaCurierat.Soferi> drivers2 = new List<Models.FirmaCurierat.Soferi>();
+                   // drivers2 = drivers;
+                   // drivers = new List<Models.FirmaCurierat.Soferi>();
+                    FirmaCurierat.Models.FirmaCurierat.Clienti data2 = new Models.FirmaCurierat.Clienti();
+                   // drivers2.RemoveAll(d => d.id_sofer == data2.id_sofer);
+                    data2 = (Models.FirmaCurierat.Clienti)data;
+                    SqlConnection scn = new SqlConnection();
+                    scn.ConnectionString = @"Data Source=DESKTOP-I3NIEPL\SQLEXPRESS;Initial Catalog=login_database;database=CurieratVladProiect;integrated security=SSPI";
+                    SqlCommand scmd = new SqlCommand("select id_comanda from comenzi where id_client = @id", scn);
+                    scmd.Parameters.Clear();
+                    scmd.Parameters.AddWithValue("@id", data2.id_client);
+                    scn.Open();
+                    int deletType = Convert.ToInt32(scmd.ExecuteScalar());
+                    scmd = new SqlCommand("delete from tipul_comenzii where id_comanda = @id", scn);
+                    scmd.Parameters.AddWithValue("@id", deletType);
+                    scmd.ExecuteNonQuery();
+                    scmd = new SqlCommand("delete from comenzi where id_comanda = @id", scn);
+                    scmd.Parameters.AddWithValue("@id", deletType);
+                    scmd.ExecuteNonQuery();
+                    scmd = new SqlCommand("delete from clienti where id_client = @id", scn);
+                    scmd.Parameters.AddWithValue("@id", data2.id_client);
+                    scmd.ExecuteNonQuery();
+                    this.StateHasChanged();
+
+                }
+            }
+            catch (System.Exception rlvMailerDeleteMailException)
+            {
+                NotificationService.Notify(NotificationSeverity.Error, $"Error", $"Unable to delete Mail");
+            }
         }
     }
 }
--- FirmaCurierat/Pages/Counter.razor.cs
+++ FirmaCurierat/Pages/Counter.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Radzen;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,14 +36,36 @@ namespace FirmaCurierat.Pages
             drivers = await dataHelper.LoadData<FirmaCurierat.Models.FirmaCurierat.Soferi, dynamic>(sqlCommand, new { }, ConnectionString);
         }
 
-        protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, dynamic data)
+     protected Syncfusion.Blazor.Grids.SfGrid<FirmaCurierat.Models.FirmaCurierat.Soferi> grid0
+        {
+            get;
+            set;
+        }
+
+        protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, object data)
         {
 
             try
             {
-                if (await DialogService.Confirm("Are you sure you want to delete this record?") == true)
+                if (await DialogService.Confirm("Do you want to delete this record?") == true)
                 {
-                    
+                  List<Models.FirmaCurierat.Soferi> drivers2 = new List<Models.FirmaCurierat.Soferi>();
+                    drivers2 = drivers;
+                    drivers = new List<Models.FirmaCurierat.Soferi>();
+                    FirmaCurierat.Models.FirmaCurierat.Soferi data2 = new Models.FirmaCurierat.Soferi();
+                    drivers2.RemoveAll(d => d.id_sofer == data2.id_sofer);
+                    data2 = (Models.FirmaCurierat.Soferi)data;
+                    SqlConnection scn = new SqlConnection();
+                    scn.ConnectionString = @"Data Source=DESKTOP-I3NIEPL\SQLEXPRESS;Initial Catalog=login_database;database=CurieratVladProiect;integrated security=SSPI";
+                    SqlCommand scmd = new SqlCommand("delete from soferi where id_sofer =  @id", scn);
+                    scmd.Parameters.Clear();
+                    scmd.Parameters.AddWithValue("@id", data2.id_sofer);
+                    scn.Open();
+                    scmd.ExecuteNonQuery();
+                    drivers = drivers2;
+                    grid0.Refresh();
+                    this.StateHasChanged();
+                
                 }
             }
             catch (System.Exception rlvMailerDeleteMailException)
@@ -50,7 +73,6 @@ namespace FirmaCurierat.Pages
                 NotificationService.Notify(NotificationSeverity.Error, $"Error", $"Unable to delete Mail");
             }
         }
-
         protected async Task goToAdd(MouseEventArgs args)
         {
            
