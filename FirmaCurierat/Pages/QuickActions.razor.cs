b8f124551c2f536a8fa535c84a78b691dc168876
VladBreaban <vlad.breaban@relevance.ro>
2021-01-04 20:39:25 +0000

    first inner join without grid refresh after selecting another city

--- FirmaCurierat/Pages/QuickActions.razor.cs
+++ FirmaCurierat/Pages/QuickActions.razor.cs
@@ -22,5 +22,10 @@ namespace FirmaCurierat.Pages
             UriHelper.NavigateTo("/employees");
          //   var dialogResult = await DialogService.OpenAsync<Employees>("Drivers");
         }
+        public async Task viewOrders(MouseEventArgs args)
+        {
+            UriHelper.NavigateTo("/view-number");
+            //   var dialogResult = await DialogService.OpenAsync<Employees>("Drivers");
+        }
     }
 }
