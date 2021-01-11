2985b8c20c1cfd2d3b81bffe9ea106c4fd67412f
VladBreaban <vlad.breaban@relevance.ro>
2021-01-11 22:47:53 +0000

    adaugat status comanda if you insert the awb

---
+++ FirmaCurierat/Pages/InfoAboutCom.razor.cs
@@ -0,0 +1,76 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaCurierat.Pages
+{
+    public class InfoAboutComComponent:ComponentBase
+    {
+
+        [Parameter]
+        public dynamic awb { get; set; }
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected Radzen.NotificationService NotificationService { get; set; }
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+        public class infoForTheGrid
+        {
+            public string awb
+            {
+                get;
+                set;
+            }
+            public int valoare_comanda
+            {
+                get;
+                set;
+            }
+            public string data_livrare
+            {
+                get;
+                set;
+            }
+            public string nume_dispecer
+            {
+                get;
+                set;
+            }
+        }
+        public List<infoForTheGrid> interestList;
+        protected DataBaseManagement.DataManagement dataHelper
+        {
+            get;
+            set;
+        }
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            dataHelper = new DataBaseManagement.DataManagement();
+            interestList = new List<infoForTheGrid>();
+            
+            string ServerName = Environment.MachineName;
+
+            string database = "CurieratVladProiect";
+            string ConnectionString = String.Format(@"Server={0}\SQLEXPRESS;Initial Catalog={1};
+                                               Integrated Security = SSPI", ServerName, database);
+
+            //select a.nume, a.prenume from soferi a where a.id_dispecer = (select c.id_dispecer from dispeceri c inner join comenzi d on c.id_dispecer = d.id_dispecer where d.awb = '123111' )
+            string sqlCommand = "select a.nume_dispecer from dispeceri a where " +
+                               " a.id_dispecer = (select c.id_dispecer from dispeceri c inner join comenzi d on c.id_dispecer = d.id_dispecer where d.awb= '" + awb + "' and d.id_client = (select id_client from clienti e where d.id_client = e.id_client ) ) ";
+            interestList = await dataHelper.LoadData<infoForTheGrid, dynamic>(sqlCommand, new { }, ConnectionString);
+            List<string> nume_dispecer = new List<string>();
+            nume_dispecer = await dataHelper.LoadData<string, dynamic>(sqlCommand, new { }, ConnectionString);
+            sqlCommand = "select a.valoare_comanda, a.awb,a.data_livrare from comenzi a where a.awb = ' " + awb + "'";
+            
+        }
+        }
+}
--- FirmaCurierat/Pages/User.razor.cs
+++ FirmaCurierat/Pages/User.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Radzen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,11 @@ namespace FirmaCurierat.Pages
     public class UserComponent : ComponentBase
     {
         public string insertedAwb;
+        [Inject]
+        protected DialogService DialogService { get; set; }
         public async Task goToInfo(MouseEventArgs args)
         {
-
+            var dialogResult = await DialogService.OpenAsync<InfoAboutCom>("Order Information", new Dictionary<string, object>() { { "awb", insertedAwb } });
         }
     }
 }
